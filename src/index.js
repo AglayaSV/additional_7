47953cdc8e056d8d082260d7c1100ee9404ffbd4
aglaya <aglayastunkus1@gmail.com>
2018-02-28 09:37:22 +0300

    additional/feature: implement buldozer function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 module.exports =function solveSudoku(matrixFirst) {
     let standardSet = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
     let matrix = JSON.parse(JSON.stringify(matrixFirst));
+
 
     function fillSetRow(i) {
         let setRow = [];
@@ -10,6 +12,7 @@ module.exports =function solveSudoku(matrixFirst) {
             }
             setRow[setRow.length] = matrix[i][j];
         }
+
         return setRow;
     }
 
@@ -21,6 +24,8 @@ module.exports =function solveSudoku(matrixFirst) {
             }
             setCol[setCol.length] = matrix[i][j];
         }
+
+
         return setCol;
     }
 
@@ -36,10 +41,12 @@ module.exports =function solveSudoku(matrixFirst) {
                 setSection[setSection.length] = matrix[i + l][j + m];
             }
         }
+
         return setSection;
     }
 
     function fillStateMatrix() {
+
         let stateMatrix = [];
         for (let i = 0; i < 9; i++) {
             stateMatrix[i] = [];
@@ -49,6 +56,7 @@ module.exports =function solveSudoku(matrixFirst) {
                         set2 = new Set(fillSetRow(i)),
                         set3 = new Set(fillSetCol(j)),
                         set4 = new Set(fillSection(i, j));
+
 
                     let difference1 = set1.difference(set2);
                     let difference2 = difference1.difference(set3);
@@ -62,8 +70,10 @@ module.exports =function solveSudoku(matrixFirst) {
 
         }
 
+
         return stateMatrix;
     }
+
 
     Set.prototype.difference = function (setB) {
         let difference = new Set(this);
@@ -74,12 +84,16 @@ module.exports =function solveSudoku(matrixFirst) {
     };
 
 
+
+
+
     function findOnes() {
         let matrixState = fillStateMatrix();
         let solvedSells = [];
         for (let i = 0; i < 9; i++) {
             for (let j = 0; j < 9; j++) {
                 if ((typeof matrixState[i][j] === 'object') && (matrixState[i][j].length === 1)) {
+
                     let solvedCoord = [];
                     solvedCoord.push(i);
                     solvedCoord.push(j);
@@ -87,8 +101,7 @@ module.exports =function solveSudoku(matrixFirst) {
                     solvedSells.push(solvedCoord);
 
                 } else if (matrixState[i][j].length > 1) {
-
-                    for (let y = 0; y < matrixState[i][j].length; y++) {
+                      for (let y = 0; y < matrixState[i][j].length; y++) {
 
                         let counterH = 0;
                         let counterV = 0;
@@ -96,18 +109,16 @@ module.exports =function solveSudoku(matrixFirst) {
                         for (let z = 0; z < 9; z++) {
                             if (typeof matrixState[i][z] !== 'object') {
 
-                             } else if (Array.from(matrixState[i][z]).indexOf(matrixState[i][j][y]) !== -1) {
+                            } else if (Array.from(matrixState[i][z]).indexOf(matrixState[i][j][y]) !== -1) {
+                                counterH++;
+                            }
+                            if (typeof matrixState[z][j] !== 'object') {
 
-                             counterH++;
+                            } else if (Array.from(matrixState[z][j]).indexOf(matrixState[i][j][y]) !== -1) {
 
-                             }
-                             if (typeof matrixState[z][j] !== 'object') {
+                                counterV++;
 
-                             } else if (Array.from(matrixState[z][j]).indexOf(matrixState[i][j][y]) !== -1) {
-
-                             counterV++;
-
-                             }
+                            }
 
                         }
 
@@ -143,10 +154,12 @@ module.exports =function solveSudoku(matrixFirst) {
         }
 
         return solvedSells;
+
     }
 
     function solveSudokeIterable() {
         let solveSellsOne = findOnes();
+        //console.log('solveSellsOne  ' + solveSellsOne);
         if (solveSellsOne.length !== 0) {
             for (let k = 0; k < solveSellsOne.length; k++) {
                 let i = solveSellsOne[k][0];
@@ -164,30 +177,78 @@ module.exports =function solveSudoku(matrixFirst) {
                 return matrix;
             } else {
 
-                let stateArrayFull = fillStateMatrix();
 
-                let stateArray = [];
 
-                for (let i=0; i<stateArrayFull.length; i++){
-                    if (typeof stateArrayFull[i] === 'object'){
-                        
-                        stateArray.push(stateArrayFull[i]);
-                    }
-                }
-
-                return matrix + ' require advanced algorithm';
+                return bulldozer();
             }
         }
     }
+
+    function bulldozer() {
+        let matrixWork = JSON.parse(JSON.stringify(matrix));
+
+        function checkSell(num, row, col) {
+            for (let i = 0; i < 9; i++) {
+                let b_index = ((Math.floor(row / 3) * 3) + Math.floor(i / 3)) * 9 + (Math.floor(col / 3) * 3) + (i % 3);
+                if (num === matrixWork[(row * 9) + i] ||
+                    num === matrixWork[col + (i * 9)] ||
+                    num === matrixWork[b_index]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        function checkAndSetSell(index) {
+            if (index >= matrixWork.length) {
+                return true;
+            } else if (matrixWork[index] !== 0) {
+                return checkAndSetSell(index + 1);
+            }
+
+            for (let i = 1; i <= 9; i++) {
+                if (checkSell(i, Math.floor(index / 9), index % 9)) {
+                    matrixWork[index] = i;
+                    if (checkAndSetSell(index + 1)) {
+                        return true;
+                    }
+                }
+            }
+
+            matrixWork[index] = 0;
+            return false;
+        }
+
+        function formatSquare(arr) {
+            let result = [];
+            for (let i = 0; i < arr.length; i += 9) {
+                result.push(arr.slice(i, i + 9));
+            }
+            console.log(result);
+            return result;
+        }
+
+     let   mainStart = function (puzzle) {
+            matrixWork = puzzle.reduce(function (a, b) {
+                return a.concat(b);
+            });
+
+            return !checkAndSetSell(0) ? matrixWork : formatSquare(matrixWork);
+        };
+        return mainStart(matrix);
+    }
+
+
 
     function isSolvedSudoku() {
         let sudoku = JSON.parse(JSON.stringify(matrix));
         for (let i = 0; i < 9; i++) {
             let [r, c] = [Math.floor(i / 3) * 3, (i % 3) * 3];
             if (
-                (sudoku[i].reduce((s, v) => s.add(v), new Set()).size != 9) ||
-                (sudoku.reduce((s, v) => s.add(v[i]), new Set()).size != 9) ||
-                (sudoku.slice(r, r + 3).reduce((s, v) => v.slice(c, c + 3).reduce((s, v) => s.add(v), s), new Set()).size != 9)
+                (sudoku[i].reduce((s, v) => s.add(v), new Set()).size !== 9) ||
+                (sudoku.reduce((s, v) => s.add(v[i]), new Set()).size !== 9) ||
+                (sudoku.slice(r, r + 3).reduce((s, v) => v.slice(c, c + 3).reduce((s, v) => s.add(v), s), new Set()).size !== 9)
             ) return false;
         }
         return matrixFirst.every((row, rowIndex) => {
@@ -196,7 +257,8 @@ module.exports =function solveSudoku(matrixFirst) {
             });
         });
     }
-    console.log(solveSudokeIterable());
+
     return solveSudokeIterable();
 }
+
 
